9ef2acc4773e59582d1bf4acd0faebffe8d577f2
110213041 <s110213041@mail1.ncnu.edu.tw>
2023-11-25 07:27:49 +0800

    temporary backend

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,8 +1,137 @@
+import { database, type item } from "./database.ts";
+
+function getItem(id: number) {
+  console.log(`INFO: item id: ${id}`);
+
+  if (!Number.isInteger(id)) return null; // early return
+
+  const queryResult = database.query<
+    [number, string, number, string, number, number, string]
+  >(
+    `--sql
+    SELECT
+      item.id, "display_name", "price", "description", "owner_id", "state",member.user_name
+    FROM
+      item
+      LEFT JOIN member ON member.id = item.owner_id
+    WHERE item.id = ?`,
+    [id],
+  );
+
+  console.log(`INFO: query result length: ${queryResult.length}`);
+
+  let result: {
+    status: "success";
+    result: item & { owner_name: string };
+  } | {
+    status: "fail";
+    message: string;
+  };
+
+  if (queryResult.length > 1) {
+    return 500;
+  }
+
+  if (queryResult.length === 1) {
+    result = {
+      status: "success",
+      result: {
+        id: queryResult[0][0],
+        display_name: queryResult[0][1],
+        price: queryResult[0][2],
+        description: queryResult[0][3],
+        owner_id: queryResult[0][4],
+        state: queryResult[0][5],
+        owner_name: queryResult[0][6],
+      },
+    };
+  } else {
+    result = {
+      status: "fail",
+      message: "item not found",
+    };
+  }
+
+  return result;
+}
+
+function getItemAll() {
+  console.log("INFO: getall");
+
+  const queryResult = database.query<
+    [number, string, number, string, number, number, string]
+  >(
+    `--sql
+    SELECT
+      item.id, "display_name", "price", "description", "owner_id", "state", member.user_name
+    FROM
+      item
+      LEFT JOIN member ON member.id = item.owner_id`,
+  );
+
+  console.log(queryResult[0]);
+
+  let result: item[] = [];
+  if (queryResult.length > 0) {
+    result = [...queryResult].map((arr) => {
+      return {
+        id: arr[0],
+        display_name: arr[1],
+        price: arr[2],
+        description: arr[3],
+        owner_id: arr[4],
+        state: arr[5],
+        owner_name: arr[6],
+      };
+    });
+  }
+
+  return result;
+}
+
 export default async function apiController(req: Request): Promise<Response> {
-  return await new Response("API entry", {
-    status: 200,
+  const url = new URL(req.url);
+  console.log(`GET: /api call`);
+  const pathName = url.pathname.replace("/api", "");
+  console.log(`INFO: api path: ${pathName}`);
+
+  switch (pathName) {
+    case "/item": {
+      const itemId = parseInt(url.searchParams.get("q") ?? "");
+      const itemActon = url.searchParams.get("a");
+      if (itemActon === null) {
+        const getItemResult = getItem(itemId);
+        if (getItemResult === null) break; // early jump to final error handling
+        else if (getItemResult === 500) {
+          return new Response(JSON.stringify({ status: 500 }), {
+            status: 500,
+            headers: {
+              "content-type": "application/json",
+            },
+          });
+        }
+        return new Response(JSON.stringify(getItemResult), {
+          status: 200,
+          headers: {
+            "content-type": "application/json",
+          },
+        });
+      } else if (itemActon === "getall" && url.searchParams.get("q") === null) {
+        return new Response(JSON.stringify(getItemAll()), {
+          status: 200,
+          headers: {
+            "content-type": "application/json",
+          },
+        });
+      }
+    }
+  }
+
+  // default response
+  return new Response(JSON.stringify({ status: 400 }), {
+    status: 400,
     headers: {
-      "content-type": "text/plain",
+      "content-type": "application/json",
     },
   });
 }
