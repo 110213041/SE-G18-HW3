c1074f60d7cb58b39bf567b1f933abcf1dc58cac
110213041 <s110213041@mail1.ncnu.edu.tw>
2023-11-25 07:28:27 +0800

    small view update

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,12 +1,23 @@
+<script lang="ts" setup>
+import { RouterView } from 'vue-router';
+import Cart from './components/Cart.vue';
+
+</script>
+
 <template>
-    <h1>Testing from App.vue</h1>
-    <router-view></router-view>
+  <h1>Testing from App.vue</h1>
+  <Cart></Cart>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </nav>
+  <RouterView></RouterView>
 </template>
 
 <style>
-
-*, *::before, *::after {
-    box-sizing: border-box;
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
 }
-
 </style>
--- src/client/view/Home.vue
+++ src/client/view/Home.vue
@@ -1,3 +1,54 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router';
+
+const isLoading = ref(true);
+
+type item = {
+  id: number;
+  display_name: string;
+  price: number;
+  description: string;
+  owner_id: number;
+  owner_name: string;
+};
+
+let itemArray: item[] = [];
+
+function getData() {
+  const queryUrl = new URL(location.origin);
+  queryUrl.pathname = "/api/item";
+  queryUrl.searchParams.set("a", "getall");
+
+  fetch(queryUrl.href)
+    .then(res => res.json())
+    .then(res => {
+      itemArray = res;
+      isLoading.value = false
+      console.log(itemArray)
+    })
+}
+
+getData()
+
+
+</script>
+
 <template>
-    <h2>This is Home</h2>
+  <h2>This is Home</h2>
+  <template v-if="!isLoading">
+    <div id="item-list-wrapper" v-if="itemArray.length > 0">
+      <div class="item-wrapper" v-for="currentItem in itemArray">
+        <div class="item-name">{{ // @ts-ignore
+          currentItem.display_name }}</div>
+
+        <div class="item-price">{{ // @ts-ignore
+          currentItem.price }}</div>
+
+        <RouterLink :to=" //@ts-ignore
+          `/item/${currentItem.id}`">to item</RouterLink>
+      </div>
+
+    </div>
+  </template>
 </template>
