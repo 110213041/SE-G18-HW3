d97f4eca035118f7b5ff5b509b202ef93472c9dc
110213041 <s110213041@mail1.ncnu.edu.tw>
2024-01-06 01:21:31 +0800

    split account into controller and model

--- src/server/controller/c_account.ts
+++ src/server/controller/c_account.ts
@@ -1,40 +1,10 @@
 import * as util from "../util.ts";
 import * as DB from "../model/database.ts";
-
+import * as AccountModel from "../model/m_account.ts";
 type login_request = {
   name: string;
   password: string;
 };
-
-function loginProcess(name: string, password: string) {
-  return DB.database.transaction(() => {
-    const query = DB.database.prepareQuery(`--sql
-      SELECT "id" FROM member WHERE
-        "user_name" = ? 
-        AND "password" = ?
-    `);
-
-    const result = query.first([name, password]);
-    if (result === undefined) {
-      return null;
-    }
-
-    const userId = result[0] as number;
-    const newSessionId = crypto.randomUUID();
-    const lifeTime = Date.now() + 60 * 60 * 24 * 365 * 1000;
-
-    DB.database.prepareQuery(`--sql
-      INSERT INTO "session" ("user_id", "session", "life_time")
-      VALUES (?, ?, ?)
-    `).execute([userId, newSessionId, lifeTime]);
-
-    return {
-      user_id: userId,
-      session: newSessionId,
-      life_time: lifeTime,
-    };
-  });
-}
 
 async function loginHandler(req: Request) {
   if (util.isPostJson(req)) return util.statusResponse(400);
@@ -44,19 +14,22 @@ async function loginHandler(req: Request) {
       await util.getRequestBody(req),
     );
 
-    const loginResult = loginProcess(loginRequest.name, loginRequest.password);
-
-    if (loginResult === null) {
+    const user = AccountModel.getIdByUserPassword(
+      loginRequest.name,
+      loginRequest.password,
+    );
+    if (user === undefined) {
       return util.statusResponse(403);
+    }
+
+    const newSession = AccountModel.createNewSession(user.id);
+    if (newSession === null) {
+      return util.statusResponse(500);
     }
 
     return util.responseTemplate({
       type: "login_response",
-      content: {
-        user_id: loginResult.user_id,
-        session: loginResult.session,
-        life_time: loginResult.life_time,
-      },
+      content: newSession,
     }, 200);
   } catch (e) {
     console.error(e);
@@ -71,23 +44,16 @@ type register_request = {
 };
 
 function registerProcess(payload: register_request) {
-  return DB.database.transaction(() => {
-    const query = DB.database.prepareQuery(`--sql
-      SELECT id FROM member WHERE user_name = ? OR email = ?
-    `);
+  const user = AccountModel.getIdByUserEmail(payload.name, payload.email);
+  if (user !== undefined) {
+    return false;
+  }
 
-    const result = query.first([payload.name, payload.email]);
-    if (result !== undefined) {
-      return false;
-    }
-
-    DB.database.prepareQuery(`--sql
-      INSERT INTO member ("user_name", "email", "password")
-      VALUES (?, ?, ?)
-    `).execute([payload.name, payload.email, payload.password]);
-
-    return true;
-  });
+  return AccountModel.createNewAccount(
+    payload.name,
+    payload.email,
+    payload.password,
+  );
 }
 
 async function registerHandler(req: Request) {
@@ -120,46 +86,17 @@ type info_request = {
   session: string;
 };
 
-export function isSessionValid(id: number, session: string): boolean {
-  const result = DB.database.prepareQuery<[number, number]>(
-    `SELECT user_id, life_time FROM "session" WHERE "session" = ?`,
-  ).first([session]);
-  if (result === undefined) {
-    return false;
-  }
-
-  const [userId, lifeTime] = result;
-
-  if (userId === id && (Date.now() + 60 * 1000 < lifeTime)) {
-    return true;
-  }
-
-  return false;
-}
-
 function infoProcess(id: number) {
-  const [name, email] = DB.database.prepareQuery<[string, string]>(
-    `SELECT user_name, email FROM member WHERE id = ?`,
-  ).first([id])!;
-
-  const roleString = DB.database.prepareQuery<[string]>(
-    `SELECT GROUP_CONCAT("role") FROM member_role WHERE user_id = ? GROUP BY user_id`,
-  ).first([id]);
-
-  let roles: number[];
-
-  if (roleString !== undefined) {
-    roles = roleString[0].split(",").map((v) => parseInt(v));
-  } else {
-    roles = [];
+  const userInfo = AccountModel.getUserById(id);
+  if (userInfo === undefined) {
+    console.error(`id=${id} don't have matched user`);
+    return undefined;
   }
 
   return {
-    name: name,
-    email: email,
+    ...userInfo,
     role: {
-      seller: roles.findIndex((v) => v === 1) !== -1,
-      shipper: roles.findIndex((v) => v === 2) !== -1,
+      ...AccountModel.getUserRoleById(id),
     },
   };
 }
@@ -176,13 +113,18 @@ async function infoHandler(req: Request) {
       infoRequest.session === undefined
     ) return util.statusResponse(403);
 
-    if (!isSessionValid(infoRequest.id, infoRequest.session)) {
+    if (!AccountModel.isSessionValid(infoRequest.id, infoRequest.session)) {
+      return util.statusResponse(403);
+    }
+
+    const userInfo = infoProcess(infoRequest.id);
+    if (userInfo === undefined) {
       return util.statusResponse(403);
     }
 
     return util.responseTemplate({
       type: "user_info",
-      content: infoProcess(infoRequest.id),
+      content: userInfo,
     }, 200);
   } catch (e) {
     console.error(e);
---
+++ src/server/model/m_account.ts
@@ -0,0 +1,136 @@
+import * as DB from "../model/database.ts";
+
+export function getIdByUserPassword(name: string, password: string) {
+  const query = DB.database.prepareQuery<
+    never,
+    { id: number },
+    [string, string]
+  >(`--sql
+      SELECT "id" FROM member WHERE
+        "user_name" = ? 
+        AND "password" = ?
+    `);
+
+  const result = query.firstEntry([name, password]);
+  query.finalize();
+
+  return result;
+}
+
+export function getIdByUserEmail(name: string, email: string) {
+  const query = DB.database.prepareQuery<
+    never,
+    { id: number },
+    [string, string]
+  >(`--sql
+      SELECT id FROM member WHERE
+      user_name = ? 
+      AND email = ?
+    `);
+
+  const result = query.firstEntry([name, email]);
+  query.finalize();
+
+  return result;
+}
+
+export function createNewAccount(
+  name: string,
+  email: string,
+  password: string,
+) {
+  try {
+    DB.database.transaction(() => {
+      const query = DB.database.prepareQuery<
+        never,
+        never,
+        [string, string, string]
+      >(`--sql
+        INSERT INTO member ("user_name", "email", "password")
+        VALUES (?, ?, ?)
+    `);
+      query.execute([name, email, password]);
+      query.finalize();
+    });
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+
+  return true;
+}
+
+export function createNewSession(userId: number) {
+  const newSessionId = crypto.randomUUID();
+  const lifeTime = Date.now() + 60 * 60 * 24 * 365 * 1000;
+
+  try {
+    const query = DB.database.prepareQuery<
+      never,
+      never,
+      [number, string, number]
+    >(`--sql
+    INSERT INTO "session" ("user_id", "session", "life_time")
+    VALUES (?, ?, ?)
+    `);
+    query.execute([userId, newSessionId, lifeTime]);
+    query.finalize();
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+
+  return {
+    user_id: userId,
+    session: newSessionId,
+    life_time: lifeTime,
+  };
+}
+
+export function isSessionValid(id: number, session: string): boolean {
+  const query = DB.database.prepareQuery<
+    never,
+    { user_id: number; life_time: number },
+    [string]
+  >(
+    `SELECT user_id, life_time FROM "session" WHERE "session" = ?`,
+  );
+
+  const result = query.firstEntry([session]);
+  query.finalize();
+
+  if (result === undefined) {
+    return false;
+  }
+
+  return (result.user_id === id && (Date.now() + 60 * 1000 < result.life_time));
+}
+
+export function getUserById(id: number) {
+  const query = DB.database.prepareQuery<
+    never,
+    { user_name: string; email: string },
+    [number]
+  >(
+    `SELECT user_name, email FROM member WHERE id = ?`,
+  );
+
+  const result = query.firstEntry([id]);
+  query.finalize();
+
+  return result;
+}
+
+export function getUserRoleById(id: number) {
+  const query = DB.database.prepareQuery<never, { role: number }, [number]>(
+    `SELECT user_name, email FROM member WHERE id = ?`,
+  );
+
+  const result = query.allEntries([id]);
+  query.finalize();
+
+  return {
+    seller: result.findIndex((v) => v.role === 1) !== -1,
+    shipper: result.findIndex((v) => v.role === 2) !== -1,
+  };
+}
