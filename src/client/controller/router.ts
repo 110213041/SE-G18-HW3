221963ac0cf105ac24827048d74723c8f2002b18
LAPTOP-MM9C4T6O\laura <tiehh29@gmail.com>
2024-01-07 20:33:36 +0800

    Adjust the login page to display normally.

--- src/client/controller/router.ts
+++ src/client/controller/router.ts
@@ -5,6 +5,7 @@ import AboutVue from "../view/About.vue";
 import NotFoundVue from "../view/404.vue";
 import ItemVue from "../view/Item.vue";
 import SellerVue from "../view/Seller.vue";
+import loginVue from "../view/login.vue";
 
 export const router = createRouter({
   history: createWebHistory(),
@@ -13,6 +14,7 @@ export const router = createRouter({
     { path: "/about", component: AboutVue },
     { path: "/item/:id", component: ItemVue },
     { path: "/seller", component: SellerVue },
+    { path: "/login", component: loginVue },
     { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFoundVue },
   ],
 });
--- src/client/view/login.vue
+++ src/client/view/login.vue
@@ -21,7 +21,21 @@
 </template>
 
 <script>
-// 麻煩家輝了
+// 登入請求
+fetch('http://127.0.0.1:8000/api/account/login', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    name: 'new_account',
+    password: 'testing132',
+  }),
+})
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
+
 </script>
 
 <style scoped>
