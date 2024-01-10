8a918b07e7be619835c42a8621068081e478b418
110213041 <s110213041@mail1.ncnu.edu.tw>
2024-01-10 14:29:28 +0800

    items.ts type

--- src/client/controller/items.ts
+++ src/client/controller/items.ts
@@ -1,102 +1,129 @@
-import { ref, onMounted } from 'vue';
-import { username, password, email, userId, session, userInfo} from "../model/global_state";
+import { onMounted, ref } from "vue";
+import {
+  email,
+  password,
+  session,
+  userId,
+  userInfo,
+  username,
+} from "../model/global_state";
 
 export type requestInfo = {
-  itemId: number,
-  quantity: number,
-  attribute,
-  value,
+  itemId: number;
+  quantity: number;
 };
 
+// Get one item info
+export const getItemInfo = async (itemId: requestInfo) => {
+  try {
+    const response = await fetch(`${window.location.origin}/api/items/get`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user_id: userId,
+        session: session,
+        item_id: itemId,
+      }),
+    });
 
-  // Get one item info
-export const getItemInfo = async(itemId:requestInfo) => {
-    try {
-      const response = await fetch(`${window.location.origin}/api/items/get`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          user_id: userId,
-          session: session,
-          item_id: itemId,
-        }),
-      });
-  
-      if (!response.ok) {
-        throw new Error(`Failed to get item info: ${response.statusText}`);
-      }
-  
-      const data = await response.json();
-      if (data.type === 'items') {
-        const itemInfo = data.content;
-        console.log('Item Info:', itemInfo);
-      } else {
-        throw new Error(`Unexpected response type: ${data.type}`);
-      }
-    } catch (error) {
-      console.error('Error during getItemInfo:', error);
+    if (!response.ok) {
+      throw new Error(`Failed to get item info: ${response.statusText}`);
     }
-  }
-  
-  // Get all items
-export const getAllItems= async() => {
-    try {
-      const response = await fetch(`${window.location.origin}/api/items/all`, {
-        method: 'GET',
-      });
-  
-      if (!response.ok) {
-        throw new Error(`Failed to get all items: ${response.statusText}`);
-      }
-  
-      const data = await response.json();
-      if (data.type === 'items_all') {
-        const allItems = data.content;
-        console.log('All Items:', allItems);
-      } else {
-        throw new Error(`Unexpected response type: ${data.type}`);
-      }
-    } catch (error) {
-      console.error('Error during getAllItems:', error);
+
+    const data = await response.json();
+    if (data.type === "items") {
+      const itemInfo = data.content;
+      console.log("Item Info:", itemInfo);
+      return itemInfo;
+    } else {
+      throw new Error(`Unexpected response type: ${data.type}`);
     }
+  } catch (error) {
+    console.error("Error during getItemInfo:", error);
   }
-  
-  // Modify item status
-export const modifyItemStatus = async(itemId:requestInfo, attribute, value) => {
-    try {
-      const response = await fetch(`${window.location.origin}/api/items/alter`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          user_id: userId,
-          session: session,
-          item_id: itemId,
-          attribute: attribute,
-          value: value,
-        }),
-      });
-  
-      if (!response.ok) {
-        throw new Error(`Failed to modify item status: ${response.statusText}`);
-      }
-  
-      console.log('Item status modified successfully');
-    } catch (error) {
-      console.error('Error modify ItemS tatus:', error);
+
+  return undefined;
+};
+
+// Get all items
+export const getAllItems = async () => {
+  try {
+    const response = await fetch(`${window.location.origin}/api/items/all`, {
+      method: "GET",
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to get all items: ${response.statusText}`);
     }
+
+    const data = await response.json();
+    if (data.type === "items_all") {
+      const allItems = data.content;
+      console.log("All Items:", allItems);
+      return allItems;
+    } else {
+      throw new Error(`Unexpected response type: ${data.type}`);
+    }
+  } catch (error) {
+    console.error("Error during getAllItems:", error);
   }
-  
-  // Create item
-export const createItem= async(itemName, itemPrice, itemDescription) => { 
+
+  return undefined;
+};
+
+type modifyItemParams =
+  | ["status", 0 | 1]
+  | ["price", number]
+  | ["display_name", string]
+  | ["description", string];
+
+// Modify item status
+export const modifyItemStatus = async (
+  itemId: requestInfo,
+  attribute: modifyItemParams[0],
+  value: modifyItemParams[1],
+) => {
+  try {
+    const response = await fetch(`${window.location.origin}/api/items/alter`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user_id: userId,
+        session: session,
+        item_id: itemId,
+        attribute: attribute,
+        value: value,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to modify item status: ${response.statusText}`);
+    }
+
+    console.log("Item status modified successfully");
+    return true;
+  } catch (error) {
+    console.error("Error modify Items status:", error);
+  }
+
+  return false;
+};
+
+// Create item
+export const createItem = async (
+  itemName: string,
+  itemPrice: number,
+  itemDescription: string,
+) => {
   try {
     const response = await fetch(`${window.location.origin}/api/items/create`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         user_id: userId,
@@ -106,21 +133,22 @@ export const createItem= async(itemName, itemPrice, itemDescription) => {
         item_description: itemDescription,
       }),
     });
-  
+
     if (!response.ok) {
       throw new Error(`Failed to create item: ${response.statusText}`);
     }
-  
+
     const data = await response.json();
-    if (data.type === 'item_create') {
+    if (data.type === "item_create") {
       const createdItem = data.content;
-      console.log('Item created successfully:', createdItem);
+      console.log("Item created successfully:", createdItem);
+      return createdItem;
     } else {
       throw new Error(`Unexpected response type: ${data.type}`);
     }
-    } catch (error) {
-      console.error('Error create Item:', error);
-    }
+  } catch (error) {
+    console.error("Error create Item:", error);
   }
-  
-  
+
+  return undefined;
+};
